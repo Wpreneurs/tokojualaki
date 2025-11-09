9d9d1727e1136d3113538bfa8818a9229e1839cc
Wpreneurs <114202814+Wpreneurs@users.noreply.github.com>
2025-11-09 07:29:32 +0700

    Refactor service worker event listeners

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ self.addEventListener("install", e => {
         "/?m=1",  // versi mobile
         "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css",
         "https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap",
-        "https://blogger.googleusercontent.com/img/.../LOGO-SIMPELTOKO-Square-300x300.jpg"
+        "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiaxkOff237mrbExBXCQrKbCLNKRAq7UeXqsy6AUCvQq7NPYwkncDWHnnZUyH1a3uQU8V7AGh_ft0ugpY9cNT3BglJBldL2hoSs7DWn9xWcoOr1GS9fe9Bn4EjfglCzP55Pd_g1ezwUYjNCA9E9sF5ZJ1AfJHBsNurN3F8xPyxsOyg5HQzZJENKe_tx5fBC/s16000/AVvXsEjTlrF_E44ChZz6v-dlSmj-HOIA%20-%20Copy.jpg"
       ]);
     })
   );
@@ -19,3 +19,4 @@ self.addEventListener("fetch", e => {
     })
   );
 });
+
